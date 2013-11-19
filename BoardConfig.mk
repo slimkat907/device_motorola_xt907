4226d22ace0bff72460cbf540761ea0239ec3b0c
ValidusCM <validuscm@gmail.com>
2013-11-19 18:22:13 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,9 +1,9 @@
 -include device/motorola/msm8960-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
--include vendor/motorola/xt926/BoardConfigVendor.mk
+-include vendor/motorola/xt907/BoardConfigVendor.mk
 
-LOCAL_PATH := device/motorola/xt926
+LOCAL_PATH := device/motorola/xt907
 
 # Use common resources
 TARGET_USES_MOTOROLA_MSM8960_COMMON_LIBLIGHT := true
@@ -15,7 +15,7 @@ BOARD_HAVE_NFC := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
 #TWRP
-DEVICE_RESOLUTION := 720x1280
+DEVICE_RESOLUTION := 540x960
 
 # userdata size is set for maxx but we don't flash anyway
 BOARD_USERDATAIMAGE_PARTITION_SIZE :=  17179869184
